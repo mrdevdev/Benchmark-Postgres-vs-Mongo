8312da13df96a1f3d33a11fb7868db9cac9b2122
Ignacio Mendez <ignaciomendez8080@gmail.com>
2021-07-05 16:11:23 -0300

    Mongo 100% finished

--- mongo.py
+++ mongo.py
@@ -1,7 +1,6 @@
 from pymongo import MongoClient
 import time
 
-
 def setup_mongo():
     mongo_client = MongoClient("mongodb://localhost:27017/")
     database_mongo = mongo_client["final_bases2"]
@@ -22,20 +21,79 @@ def mongo_query_6(dataset):
 
 
 def mongo_query_1():
-    pass
+    mongo_client = MongoClient("mongodb://localhost:27017/")
+    database_mongo = mongo_client["final_bases2"]
+    collection = database_mongo["users"]
+    
+    timer_start = time.time()
+    collection.aggregate([{
+        "$group": {
+            "_id": "$company.companyCar.Model", 
+            "cars_by_quantity": {"$sum": 1}
+        }
+    }, {
+        "$sort": {"cars_by_quantity": -1}
+    }])
+    timer_end = time.time()
+    print("\tQuery 1 - Mongo: {0} segundos".format(timer_end - timer_start))
 
 
 def mongo_query_2():
-    pass
+    mongo_client = MongoClient("mongodb://localhost:27017/")
+    database_mongo = mongo_client["final_bases2"]
+    collection = database_mongo["users"]
+    
+    timer_start = time.time()
+    collection.update_many(
+    {},
+    {
+        "$set": {"company.companyCar.Category": "SUV"}
+    })
+    timer_end = time.time()
+    print("\tQuery 2 - Mongo: {0} segundos".format(timer_end - timer_start))
 
 
 def mongo_query_3():
-    pass
+    mongo_client = MongoClient("mongodb://localhost:27017/")
+    database_mongo = mongo_client["final_bases2"]
+    collection = database_mongo["users"]
+    
+    timer_start = time.time()
+    collection.update_many(
+    {},
+    {
+        "$set" : {"company.companyCar.licensePlate": "000-123"}
+    })
+    timer_end = time.time()
+    print("\tQuery 3 - Mongo: {0} segundos".format(timer_end - timer_start))
 
 
 def mongo_query_4():
-    pass
+    mongo_client = MongoClient("mongodb://localhost:27017/")
+    database_mongo = mongo_client["final_bases2"]
+    collection = database_mongo["users"]
+    
+    timer_start = time.time()
+    collection.update_many(
+    {"company.companyCar.Year": {"$gt" : 2005}},
+    {
+        "$set": {"company.companyCar.Make": "Ford"}
+    })
+    timer_end = time.time()
+    print("\tQuery 4 - Mongo: {0} segundos".format(timer_end - timer_start))
 
 
 def mongo_query_5():
-    pass
+    mongo_client = MongoClient("mongodb://localhost:27017/")
+    database_mongo = mongo_client["final_bases2"]
+    collection = database_mongo["users"]
+    
+    timer_start = time.time()
+    collection.update_many(
+    {"company.companyCar.Year": {"$lt" : 2005}},
+    {
+        "$set": {"company.companyCar.color": "Yellow"}
+    })
+    timer_end = time.time()
+    print("\tQuery 5 - Mongo: {0} segundos".format(timer_end - timer_start))
+
