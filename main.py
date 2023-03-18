56433f00a640ee5fa6a6b4436157ab6d4825184b
AlexT <lxtroy@gmail.com>
2023-03-17 18:39:28 -0700

    ch

--- main.py
+++ main.py
@@ -7,92 +7,109 @@ import time
 import argparse
 
 
-CANTIDAD_DE_REGISTROS = 10000
-
-
 def set_username(name):
-    name_split = name.split()
-    return name_split[0][0] + name_split[1]
+	name_split = name.split()
+	return name_split[0][0] + name_split[1]
 
 
 def set_mail(username, mail):
-    mail_split = mail.split('@')
-    return username + '@' + mail_split[1]
+	mail_split = mail.split('@')
+	return username + '@' + mail_split[1]
 
 
 def main():
 
-    # PARSE ARGUMENTS
-    parser = argparse.ArgumentParser()
-    parser.add_argument('-pg_username', dest='postgres_username', required=True)
-    parser.add_argument('-pg_password', dest='postgres_password', required=True)
-    args = parser.parse_args()
-    postgres_username = str(args.postgres_username)
-    postgres_password = str(args.postgres_password)
+	CANTIDAD_DE_REGISTROS = 100000
+	PG_USERNAME = "postgres"
+	PG_PASSWORD = ""
+	PG_HOST = ""
+	PG_DB = "postgres"
 
-    fake = Faker()
-    fake.add_provider(VehicleProvider)
+	# PARSE ARGUMENTS
+	parser = argparse.ArgumentParser()
+	parser.add_argument('-pg_username', dest='postgres_username', required=False, default=PG_USERNAME)
+	parser.add_argument('-pg_password', dest='postgres_password', required=False, default=PG_PASSWORD)
+	args = parser.parse_args()
+	postgres_username = str(args.postgres_username)
+	postgres_password = str(args.postgres_password)
 
-    dataset_string = []  # Postgres needs strings to insert data
-    dataset_dict = []    # Mongo needs dicts to insert data
+	fake = Faker()
+	fake.add_provider(VehicleProvider)
 
-    print("Generando dataset...")
-    timer_start = time.time()
-    for x in range(CANTIDAD_DE_REGISTROS):  # 100k en 100 segundos
-        company = {}
-        company['name'] = fake.company()
-        company['suffix'] = fake.company_suffix()
-        company['address'] = fake.address()
-        company['companyCar'] = fake.vehicle_object()
+	dataset_string = []  # Postgres needs strings to insert data
+	dataset_dict = []  # Mongo needs dicts to insert data
 
-        person = fake.simple_profile()
-        person['company'] = company
-        person.pop('birthdate')
-        person['username'] = set_username(person['name'])
-        person['mail'] = set_mail(person['username'], person['mail'])
+	res = None
+	try:
+		connt = psycopg2.connect(host=PG_HOST, database=PG_DB, user=postgres_username, password=postgres_password)
+		curt = connt.cursor()
+		curt.execute("SELECT * FROM usersJsonB  LIMIT 1")
+		res = curt.fetchone()
+		print(res)
+		connt.close()
+	except:
+		pass
 
-        dataset_string.append(json.dumps(person))
-        person['_id'] = x
-        dataset_dict.append(person)
+	if res is not None:
+		CANTIDAD_DE_REGISTROS = 100
 
-    timer_end = time.time()
-    print("\tTiempo en generar el dataset: {0} segundos".format(timer_end - timer_start))
+	print("Generando dataset...")
+	timer_start = time.time()
+	for x in range(CANTIDAD_DE_REGISTROS):  # 100k en 100 segundos
+		company = {}
+		company['name'] = fake.company()
+		company['suffix'] = fake.company_suffix()
+		company['address'] = fake.address()
+		company['companyCar'] = fake.vehicle_object()
 
-    # SETUP
-    print("Creando tablas...")
-    setup_postgres(postgres_username, postgres_password)
-    setup_mongo()
+		person = fake.simple_profile()
+		person['company'] = company
+		person.pop('birthdate')
+		person['username'] = set_username(person['name'])
+		person['mail'] = set_mail(person['username'], person['mail'])
 
-    # QUERY 6
-    print("QUERY #6")
-    postgres_query_6(dataset_string, postgres_username, postgres_password)
-    mongo_query_6(dataset_dict)
+		dataset_string.append(json.dumps(person))
+		person['_id'] = x
+		dataset_dict.append(person)
+	timer_end = time.time()
+	print("\tTiempo en generar el dataset: {0} segundos".format(timer_end - timer_start))
 
-    # QUERY 1
-    print("QUERY #1")
-    postgres_query_1(postgres_username, postgres_password)
-    mongo_query_1()
+	# SETUP
+	if res is None:
+		print("Creando tablas...")
+		setup_postgres(postgres_username, postgres_password, PG_HOST, PG_DB)
+		# setup_mongo()
 
-    # QUERY 2
-    print("QUERY #2")
-    postgres_query_2(postgres_username, postgres_password)
-    mongo_query_2()
+		# QUERY 6
+		print("QUERY insert")
+		postgres_query_6(dataset_string, postgres_username, postgres_password, PG_HOST, PG_DB)
+		# mongo_query_6(dataset_dict)
 
-    # QUERY 3
-    print("QUERY #3")
-    postgres_query_3(postgres_username, postgres_password)
-    mongo_query_3()
+	# QUERY 1
+	print("QUERY #1")
+	postgres_query_1(postgres_username, postgres_password, PG_HOST, PG_DB)
+	# mongo_query_1()
 
-    # QUERY 4
-    print("QUERY #4")
-    postgres_query_4(postgres_username, postgres_password)
-    mongo_query_4()
+	# QUERY 2
+	print("QUERY #2")
+	postgres_query_2(postgres_username, postgres_password, PG_HOST, PG_DB)
+	# mongo_query_2()
 
-    # QUERY 5
-    print("QUERY #5")
-    postgres_query_5(postgres_username, postgres_password)
-    mongo_query_5()
+	# QUERY 3
+	print("QUERY #3")
+	postgres_query_3(postgres_username, postgres_password, PG_HOST, PG_DB)
+	# mongo_query_3()
+
+	# QUERY 4
+	print("QUERY #4")
+	postgres_query_4(postgres_username, postgres_password, PG_HOST, PG_DB)
+	# mongo_query_4()
+
+	# QUERY 5
+	print("QUERY #5")
+	postgres_query_5(postgres_username, postgres_password, PG_HOST, PG_DB)
+	# mongo_query_5()
 
 
 if __name__ == '__main__':
-    main()
+	main()
--- postgres.py
+++ postgres.py
@@ -2,169 +2,143 @@ import psycopg2
 import time
 
 create_table_1 = "DROP TABLE IF EXISTS usersJsonB; CREATE TABLE usersJsonB(" \
-                    "userId SERIAL PRIMARY KEY," \
-                    "userData JSONB NOT NULL" \
-                 ");"
+     "userId SERIAL PRIMARY KEY," \
+     "userData JSONB NOT NULL" \
+     ");"
 
 create_table_2 = "DROP TABLE IF EXISTS usersJson; CREATE TABLE usersJson(" \
-                    "userId SERIAL PRIMARY KEY," \
-                    "userData JSON NOT NULL" \
-                 ");"
+     "userId SERIAL PRIMARY KEY," \
+     "userData JSON NOT NULL" \
+     ");"
 
 query1_string = "SELECT jsonb_extract_path(userData, 'company', 'companyCar', 'Model') as car_model, " \
-                        "count(*) as cars_by_quantity " \
-                "FROM usersJsonB " \
-                "GROUP BY jsonb_extract_path(userData, 'company', 'companyCar', 'Model')"
+      "count(*) as cars_by_quantity " \
+    "FROM usersJsonB " \
+    "GROUP BY jsonb_extract_path(userData, 'company', 'companyCar', 'Model')"
 
 query2_string = "UPDATE usersJsonB as u1 " \
-                "SET userData = (" \
-                        "SELECT jsonb_set(userData, '{company, companyCar, Category}', '\"SUV\"') " \
-                        "FROM usersJsonB as u2 " \
-                        "WHERE u1.userId = u2.userId" \
-                ");"
+    "SET userData = (" \
+      "SELECT jsonb_set(userData, '{company, companyCar, Category}', '\"SUV\"') " \
+      "FROM usersJsonB as u2 " \
+      "WHERE u1.userId = u2.userId" \
+    ");"
 
 query3_string = "UPDATE usersJsonB as u1 " \
-                "SET userData = (" \
-                        "SELECT jsonb_insert(userData, '{company, companyCar, licensePlate}', '\"000-123\"') " \
-                        "FROM usersJsonB as u2 " \
-                        "WHERE u1.userId = u2.userId" \
-                ");"
+    "SET userData = (" \
+      "SELECT jsonb_insert(userData, '{company, companyCar, licensePlate}', '\"000-123\"') " \
+      "FROM usersJsonB as u2 " \
+      "WHERE u1.userId = u2.userId" \
+    ");"
 
 query4_string = "UPDATE usersJsonB as u1 " \
-                "SET userData = (" \
-                        "SELECT jsonb_set(userData, '{company, companyCar, Make}', '\"Ford\"') " \
-                        "FROM usersJsonB as u2 " \
-                        "WHERE u1.userId = u2.userId) " \
-                "WHERE (userData #>> '{company, companyCar, Year}')::int > 2005;"
+    "SET userData = (" \
+      "SELECT jsonb_set(userData, '{company, companyCar, Make}', '\"Ford\"') " \
+      "FROM usersJsonB as u2 " \
+      "WHERE u1.userId = u2.userId) " \
+    "WHERE (userData #>> '{company, companyCar, Year}')::int > 2005;"
 
 query5_string = "UPDATE usersJsonB as u1 " \
-                "SET userData = (" \
-                        "SELECT jsonb_insert(userData, '{company, companyCar, color}', '\"yellow\"') " \
-                        "FROM usersJsonB as u2 " \
-                        "WHERE u1.userId = u2.userId) " \
-                "WHERE (userData #>> '{company, companyCar, Year}')::int < 2005;"
+    "SET userData = (" \
+      "SELECT jsonb_insert(userData, '{company, companyCar, color}', '\"yellow\"') " \
+      "FROM usersJsonB as u2 " \
+      "WHERE u1.userId = u2.userId) " \
+    "WHERE (userData #>> '{company, companyCar, Year}')::int < 2005;"
+
+steps = 1
 
 
-def postgres_query_6(dataset, username, password):
-    conn = psycopg2.connect(
-        host="localhost",
-        database="postgres",
-        user=username,
-        password=password)
+def postgres_query_6(dataset, username, password, host, dbname):
+	conn = psycopg2.connect(host=host, database=dbname, user=username, password=password)
 
-    cur = conn.cursor()
-    query = "INSERT INTO usersJson (userData) VALUES ('{0}')".format(dataset[0])
-    timer_start = time.time()
-    for x in range(1, len(dataset)):
-        query = query + ",('{0}')".format(dataset[x])
-    cur.execute(query + ";")
-    conn.commit()
-    timer_end = time.time()
-    print("\tQuery 6 - Postgres - with JSON: {0} segundos".format(timer_end - timer_start))
-    cur.close()
+	cur = conn.cursor()
+	query = "INSERT INTO usersJson (userData) VALUES ('{0}')".format(dataset[0])
+	timer_start = time.time()
+	for x in range(1, len(dataset)):
+		query = query + ",('{0}')".format(dataset[x])
+	cur.execute(query + ";")
+	conn.commit()
+	timer_end = time.time()
+	print("\tQuery 6 - Postgres - with JSON: {0} segundos".format(timer_end - timer_start))
+	cur.close()
 
-    cur = conn.cursor()
-    query = "INSERT INTO usersJsonB (userData) VALUES ('{0}'::jsonb)".format(dataset[0])
-    timer_start = time.time()
-    for x in range(1, len(dataset)):
-        query = query + ",('{0}'::jsonb)".format(dataset[x])
-    cur.execute(query + ";")
-    conn.commit()
-    timer_end = time.time()
-    print("\tQuery 6 - Postgres - with JSONB: {0} segundos".format(timer_end - timer_start))
-    cur.close()
+	cur = conn.cursor()
+	query = "INSERT INTO usersJsonB (userData) VALUES ('{0}'::jsonb)".format(dataset[0])
+	timer_start = time.time()
+	for x in range(1, len(dataset)):
+		query = query + ",('{0}'::jsonb)".format(dataset[x])
+	cur.execute(query + ";")
+	conn.commit()
+	timer_end = time.time()
+	print("\tQuery 6 - Postgres - with JSONB: {0} segundos".format(timer_end - timer_start))
+	cur.close()
 
 
-def postgres_query_1(username, password):
-    conn = psycopg2.connect(
-        host="localhost",
-        database="postgres",
-        user=username,
-        password=password)
-
-    cur = conn.cursor()
-    timer_start = time.time()
-    cur.execute(query1_string)
-    conn.commit()
-    timer_end = time.time()
-    print("\tQuery 1 - Postgres: {0} segundos".format(timer_end - timer_start))
-    cur.close()
+def postgres_query_1(username, password, host, dbname):
+	timer_start = time.time()
+	for i in range(0, steps):
+		conn = psycopg2.connect(host=host, database=dbname, user=username, password=password)
+		cur = conn.cursor()
+		cur.execute(query1_string)
+		conn.commit()
+		cur.close()
+	timer_end = time.time()
+	print("\tQuery 1 - Postgres: {0} segundos".format(timer_end - timer_start))
 
 
-def postgres_query_2(username, password):
-    conn = psycopg2.connect(
-        host="localhost",
-        database="postgres",
-        user=username,
-        password=password)
-
-    cur = conn.cursor()
-    timer_start = time.time()
-    cur.execute(query2_string)
-    conn.commit()
-    timer_end = time.time()
-    print("\tQuery 2 - Postgres: {0} segundos".format(timer_end - timer_start))
-    cur.close()
+def postgres_query_2(username, password, host, dbname):
+	timer_start = time.time()
+	for i in range(0, steps):
+		conn = psycopg2.connect(host=host, database=dbname, user=username, password=password)
+		cur = conn.cursor()
+		cur.execute(query2_string)
+		conn.commit()
+		cur.close()
+	timer_end = time.time()
+	print("\tQuery 2 - Postgres: {0} segundos".format(timer_end - timer_start))
 
 
-def postgres_query_3(username, password):
-    conn = psycopg2.connect(
-        host="localhost",
-        database="postgres",
-        user=username,
-        password=password)
-
-    cur = conn.cursor()
-    timer_start = time.time()
-    cur.execute(query3_string)
-    conn.commit()
-    timer_end = time.time()
-    print("\tQuery 3 - Postgres: {0} segundos".format(timer_end - timer_start))
-    cur.close()
+def postgres_query_3(username, password, host, dbname):
+	timer_start = time.time()
+	for i in range(0, steps):
+		conn = psycopg2.connect(host=host, database=dbname, user=username, password=password)
+		cur = conn.cursor()
+		cur.execute(query3_string)
+		conn.commit()
+		cur.close()
+	timer_end = time.time()
+	print("\tQuery 3 - Postgres: {0} segundos".format(timer_end - timer_start))
 
 
-def postgres_query_4(username, password):
-    conn = psycopg2.connect(
-        host="localhost",
-        database="postgres",
-        user=username,
-        password=password)
-
-    cur = conn.cursor()
-    timer_start = time.time()
-    cur.execute(query4_string)
-    conn.commit()
-    timer_end = time.time()
-    print("\tQuery 4 - Postgres: {0} segundos".format(timer_end - timer_start))
-    cur.close()
+def postgres_query_4(username, password, host, dbname):
+	for i in range(0, steps):
+		conn = psycopg2.connect(host=host, database=dbname, user=username, password=password)
+		cur = conn.cursor()
+		timer_start = time.time()
+		cur.execute(query4_string)
+		conn.commit()
+		cur.close()
+	timer_end = time.time()
+	print("\tQuery 4 - Postgres: {0} segundos".format(timer_end - timer_start))
 
 
-def postgres_query_5(username, password):
-    conn = psycopg2.connect(
-        host="localhost",
-        database="postgres",
-        user=username,
-        password=password)
-
-    cur = conn.cursor()
-    timer_start = time.time()
-    cur.execute(query5_string)
-    conn.commit()
-    timer_end = time.time()
-    print("\tQuery 5 - Postgres: {0} segundos".format(timer_end - timer_start))
-    cur.close()
+def postgres_query_5(username, password, host, dbname):
+	timer_start = time.time()
+	for i in range(0, steps):
+		conn = psycopg2.connect(host=host, database=dbname, user=username, password=password)
+		cur = conn.cursor()
+		cur.execute(query5_string)
+		conn.commit()
+		cur.close()
+	timer_end = time.time()
+	print("\tQuery 5 - Postgres: {0} segundos".format(timer_end - timer_start))
 
 
-def setup_postgres(username, password):
-    conn = psycopg2.connect(
-        host="localhost",
-        database="postgres",
-        user=username,
-        password=password)
-    cur = conn.cursor()
-    cur.execute(create_table_1)
-    conn.commit()
-    cur.execute(create_table_2)
-    conn.commit()
-    cur.close()
-    print("\tTablas en Postgres creadas!")
+def setup_postgres(username, password, host, dbname):
+	conn = psycopg2.connect(host=host, database=dbname, user=username, password=password)
+	cur = conn.cursor()
+	cur.execute(create_table_1)
+	conn.commit()
+	cur.execute(create_table_2)
+	conn.commit()
+	cur.close()
+	print("\tTablas en Postgres creadas!")
